91b8f1771146fbf8253abcfb3170962c6026d306
maarteti <maarteti@gmail.com>
2020-12-29 16:09:10 +0900

    add root ribs

---
+++ rib-test/RIBs/Root/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  rib-test
+//
+//  Created by albin on 2020/12/29.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol RootPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class RootViewController: UIViewController, RootPresentable, RootViewControllable {
+
+    weak var listener: RootPresentableListener?
+}
