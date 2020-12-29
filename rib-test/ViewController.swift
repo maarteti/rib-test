682e629690da67766bfae4d60d675249e2817095
maarteti <maarteti@gmail.com>
2020-12-29 15:02:40 +0900

    Initial Commit

---
+++ rib-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rib-test
+//
+//  Created by albin on 2020/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
